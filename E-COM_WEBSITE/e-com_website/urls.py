b452dabd1a03d18f3534469df10c47847308b25b
Hemmie001 <emmanuelakej@gmail.com>
2024-08-14 17:06:26 +0100

    core's url

--- E-COM_WEBSITE/e-com_website/urls.py
+++ E-COM_WEBSITE/e-com_website/urls.py
@@ -17,9 +17,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
-from core.views import index
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("", index)
+    path("", include(core.urls))
 ]
