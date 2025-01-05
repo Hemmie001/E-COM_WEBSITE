86cab07b79ad48ffbcec75e289d9319e52f27e17
Hemmie001 <emmanuelakej@gmail.com>
2025-01-05 17:00:35 +0100

    vendor list view

--- E-COM_WEBSITE/core/views.py
+++ E-COM_WEBSITE/core/views.py
@@ -142,6 +142,36 @@ def category_product_list_view(request, cid):
     }
     return render(request, "core/category-product-list.html", context)
 
+def filter_product(request):
+    categories = request.GET.getlist("category[]")
+    vendors = request.GET.getlist("vendor[]")
+
+
+    min_price = request.GET['min_price']
+    max_price = request.GET['max_price']
+
+    products = Product.objects.filter(product_status="published").order_by("-id").distinct()
+
+    products = products.filter(price__gte=min_price)
+    products = products.filter(price__lte=max_price)
+
+
+    if len(categories) > 0:
+        products = products.filter(category__id__in=categories).distinct() 
+    else:
+        products = Product.objects.filter(product_status="published").order_by("-id").distinct()
+    if len(vendors) > 0:
+        products = products.filter(vendor__id__in=vendors).distinct() 
+    else:
+        products = Product.objects.filter(product_status="published").order_by("-id").distinct()    
+    
+       
+
+    
+    data = render_to_string("core/async/product-list.html", {"products": products})
+    return JsonResponse({"data": data})
+
+
 def cart_view(request):
     cart_total_amount = 0
     if 'cart_data_obj' in request.session:
@@ -429,3 +459,93 @@ def privacy_policy(request):
 
 def terms_of_service(request):
     return render(request, "core/terms_of_service.html")
+
+def vendor_detail_view(request, vid):
+    vendor = Vendor.objects.get(vid=vid)
+    products = Product.objects.filter(vendor=vendor, product_status="published").order_by("-id")
+
+    context = {
+        "vendor": vendor,
+        "products": products,
+    }
+    return render(request, "core/vendor-detail.html", context)
+
+def product_detail_view(request, pid):
+    product = Product.objects.get(pid=pid)
+    # product = get_object_or_404(Product, pid=pid)
+    products = Product.objects.filter(category=product.category).exclude(pid=pid)
+
+    # Getting all reviews related to a product
+    reviews = ProductReview.objects.filter(product=product).order_by("-date")
+
+    # Getting average review
+    average_rating = ProductReview.objects.filter(product=product).aggregate(rating=Avg('rating'))
+
+    # Product Review form
+    review_form = ProductReviewForm()
+
+
+    make_review = True 
+
+    if request.user.is_authenticated:
+        address = Address.objects.get(status=True, user=request.user)
+        user_review_count = ProductReview.objects.filter(user=request.user, product=product).count()
+
+        if user_review_count > 0:
+            make_review = False
+    
+    address = "Login To Continue"
+
+
+    p_image = product.p_images.all()
+
+    context = {
+        "p": product,
+        "address": address,
+        "make_review": make_review,
+        "review_form": review_form,
+        "p_image": p_image,
+        "average_rating": average_rating,
+        "reviews": reviews,
+        "products": products,
+    }
+
+    return render(request, "core/product-detail.html", context)
+
+def search_view(request):
+    query = request.GET.get("q")
+
+    products = Product.objects.filter(title__icontains=query).order_by("-date")
+
+    context = {
+        "products": products,
+        "query": query,
+    }
+    return render(request, "core/search.html", context)
+
+def product_list_view(request):
+    products = Product.objects.filter(product_status="published").order_by("-id")
+    tags = Tag.objects.all().order_by("-id")[:6]
+
+    context = {
+        "products":products,
+        "tags":tags,
+    }
+
+    return render(request, 'core/product-list.html', context)
+
+def tag_list(request, tag_slug=None):
+
+    products = Product.objects.filter(product_status="published").order_by("-id")
+
+    tag = None 
+    if tag_slug:
+        tag = get_object_or_404(Tag, slug=tag_slug)
+        products = products.filter(tags__in=[tag])
+
+    context = {
+        "products": products,
+        "tag": tag
+    }
+
+    return render(request, "core/tag.html", context)
